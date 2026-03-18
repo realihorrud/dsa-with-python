3e132ec1805afe09c2c5d75f15395971dfb833b6
Ihor <Igorryd7@gmail.com>
2026-03-18 12:15:16 +0200

    Implement Quick Sort algorithm

---
+++ SortingAlgorithms/quick_sort.py
@@ -0,0 +1,33 @@
+from random import randint
+
+
+def quick_sort(lst):
+    length = len(lst)
+
+    if length <= 1:
+        return lst
+    else:
+        pivot = lst.pop()
+
+    left = []
+    right = []
+    for elem in lst:
+        if elem < pivot:
+            left.append(elem)
+        else:
+            right.append(elem)
+
+    return quick_sort(left) + [pivot] + quick_sort(right)
+
+
+data_list = [randint(1, 10_000) for i in range(1, 10)]
+print(f"Unsorted List: {data_list}")
+
+# Sort the list in descending direction
+result = quick_sort(data_list)
+
+print(result)
+
+print()
+
+print("Best Case Time Complexity:", "O(n * log(n))")
